fcab8970f52578fb7a365a0d41dcc14f2bc85635
Julie Cariou <julie.cariou2605@gmail.com>
2024-11-04 11:42:43 +0100

    App.js et implémentation du jeu

--- js/app.js
+++ js/app.js
@@ -1,16 +1,56 @@
 //APP
-let cards = ["card1", "card2", "card3", "card4", "card5"];
+//VARIABLES UTILES
+let cards = ["card1", "card2", "card3"];
+let imgCard;
+let containerCard;
+let pathImg = './asset/img/';
+let currentCard;
+let previousCard;
 
-let containerCard = document.querySelector('.container-card');
+document.addEventListener('DOMContentLoaded', () => {
+    containerCard = document.querySelector('.container-card');
+    imgCard = document.querySelector('#img-card');
 
+    afficherCard();
+});
 
 function afficherCard() {
+    let numberRandom = Math.floor(Math.random() * 3);
 
+    containerCard.style.opacity = '0';
+
+    previousCard = currentCard;
+
+    currentCard = cards[numberRandom];
+
+    imgCard.src = pathImg + currentCard + '.jpg';
+
+    imgCard.onload = () => {
+        containerCard.style.opacity = '1';
+    }
+
+    // console.log('Previous Card : ' + previousCard);
+    // console.log('Current Card : ' + currentCard);
 }
 
-function verificationCard() {
+function verificationCard(userAnswer) {
 
-}
+    let sameCard = currentCard === previousCard;
+
+    if(userAnswer === sameCard ) {
+        console.log('Bonne réponse !');
+    } else {
+        console.log('Mauvaise réponse.');
+    }
+
+    containerCard.classList.remove('animate-pass');
+    void containerCard.offsetWidth;
+    containerCard.classList.add('animate-pass');
+
+    containerCard.addEventListener('animationend', () => {
+        afficherCard();
+    }, {once: true});
+};
 
 //Jeux de carte
 
