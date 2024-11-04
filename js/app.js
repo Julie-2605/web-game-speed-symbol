97d1b8d265c92196878b93986da9a9759531e5ec
Julie Cariou <julie.cariou2605@gmail.com>
2024-11-04 15:31:59 +0100

    Icon du Statut de la Réponse Valid or False + Configuration des touches + Animations

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@
 let cards = ["card1", "card2", "card3"];
 let imgCard;
 let containerCard;
+
+let containerResponse;
+let iconResponse;
+
+let secondColor;
+let redColor;
+
 let pathImg = './asset/img/';
 let currentCard;
 let previousCard;
@@ -10,6 +17,10 @@ let previousCard;
 document.addEventListener('DOMContentLoaded', () => {
     containerCard = document.querySelector('.container-card');
     imgCard = document.querySelector('#img-card');
+    containerResponse = document.querySelector('.response-status');
+    iconResponse = document.querySelector('.response-status i');
+    secondColor = getComputedStyle(document.documentElement).getPropertyValue('--second-color');
+    redColor = getComputedStyle(document.documentElement).getPropertyValue('--red-color');
 
     afficherCard();
 });
@@ -38,28 +49,61 @@ function verificationCard(userAnswer) {
     let sameCard = currentCard === previousCard;
 
     if(userAnswer === sameCard ) {
-        console.log('Bonne réponse !');
+        // console.log('Bonne réponse !');
+
+        //Icon Font Awesome = V
+        iconResponse.className = '';
+        iconResponse.className = 'fas fa-check';
+
+       //Affichage de l'icon + color + animation
+        containerResponse.style.display = 'flex';
+        containerResponse.style.backgroundColor = secondColor;
+        containerResponse.classList.add('animate-response');
+
+        //Attendre que l'animation se finisse
+        containerResponse.addEventListener('animationend', () => {
+            containerResponse.style.display = 'none';
+            containerResponse.classList.remove('animate-response');
+        }, {once: true});
     } else {
-        console.log('Mauvaise réponse.');
+        // console.log('Mauvaise réponse.');
+
+        //Icon Font Awesome = X
+        iconResponse.className = '';
+        iconResponse.className = 'fa-solid fa-xmark';
+
+        //Affichage de l'icon + color + animation
+        containerResponse.style.display = 'flex';
+        containerResponse.style.backgroundColor = redColor;
+        containerResponse.classList.add('animate-response');
+
+        //Attendre que l'animation se finisse
+        containerResponse.addEventListener('animationend', () => {
+            containerResponse.style.display = 'none';
+            containerResponse.classList.remove('animate-response');
+        }, {once: true});
     }
 
+    //Animation des card
     containerCard.classList.remove('animate-pass');
     void containerCard.offsetWidth;
     containerCard.classList.add('animate-pass');
 
+    //Attendre que l'animation se finisse
     containerCard.addEventListener('animationend', () => {
         afficherCard();
     }, {once: true});
 };
 
-//Jeux de carte
+//CONFIGURATION DES TOUCHES
+document.addEventListener('keypress', (event) => {
+    if(event.key === 'v' || event.key === 'V') {
+        verificationCard(true);
+    } else if(event.key === 'x' || event.key === 'X') {
+        verificationCard(false);
+    }
+});
 
-//Afficher la carte Random
-
-//Première carte afficher pendant 2s
-
-//Event boutton Oui / Non if ==
 //Config des touches pour aller plus vite
-//Si la carte afficher correspond à la précédente.
-//Donc un return précédente carte en mémoire.
+
 //Système de score.
